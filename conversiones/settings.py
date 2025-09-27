c609460bae6d10e26d5e342557baeb8ab4fda465
Enrique Sardon <ensarman@gmail.com>
2025-09-27 17:58:58 -0500

    agregados los hosts a ALLOWED_HOSTS

--- conversiones/settings.py
+++ conversiones/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.getenv("DJANGO_DEPLOY") == "False" or 0 else True
+DEBUG = True if os.getenv("DJANGO_DEPLOY") == "False" or 0 else False
 
 
 ALLOWED_HOSTS = [] if DEBUG else [os.getenv("DJANGO_HOST", "localhost")]
